3653ace456f6bf9207108ba61f5904dbbdb35a5c
Daniil <vover.96@mai.ru>
2021-05-16 16:18:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var person = {
+    name: "John", age: 31
+};
+
+
+
+//var a = prompt("Привет");
+var answer = confirm("Are yo ok?");
+console.log(answer);
+
+var a = prompt("Привет");
+console.log(a);
+console.log(typeof(a));
